1a883940c015cd6730370e5e99730f9483f7838b
shawncocklin <shawncocklin@gmail.com>
2022-09-25 10:50:33 -0400

    imported db table into Category View

---
+++ BookwormWeb/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using BookwormWeb.Data;
+using BookwormWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookwormWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly AppDBContext db;
+
+        public CategoryController(AppDBContext db)
+        {
+            this.db = db;
+        }
+        
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = db.Categories;
+            return View(objCategoryList);
+        }
+    }
+}
